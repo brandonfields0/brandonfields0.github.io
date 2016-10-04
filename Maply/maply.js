0bf05fc90d3aa339fa7f6cb1f7bd6f34138eeb46
Brandon Fields <derek.fields@interapthq.com>
2016-10-04 01:14:17 -0400

    Add files via upload

--- Maply/maply.js
+++ Maply/maply.js
@@ -1,28 +1,39 @@
-var googleMap = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAPE6dciKIzg-bozKSVdR-BWdbe7XIZJ7s&callback=initMap";
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+// pull location from Google API
+function googleLoc(location) {
+var googleKey = "AIzaSyAPE6dciKIzg-bozKSVdR-BWdbe7XIZJ7s";
+var googleMap = "https://maps.googleapis.com/maps/api/js?key=" + googleKey + "&callback=initMap";
+
+// initializes Google Map w/ lat, lng
+function initMap(card) {
+  $("#map".append((card)))
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 37.1714959, lng: -83.195988 },
     zoom: 8
   });
 }
 
- $.ajax(googleMap + map).done(function (data) {
+// pulls info from Google Maps
+  $.ajax({googleMap,dataType: "json"})
+
+
+// gets zip, city location
     var googleLat = data.results[0].geometry.location.lat;
     var googleLng = data.results[0].geometry.location.lng;
     var city = data.results[0].formatted_address;
+   
+}
 
-  $.ajax("https://maps.googleapis.com/maps/api/js?key=AIzaSyAPE6dciKIzg-bozKSVdR-BWdbe7XIZJ7s&callback=initMap", 
-  {dataType: "jsonp"}).done(function(data) {
+//adds function to submit button to get info when clicked
+ $("#submit").on("click", function() {
+    var location = $('#zip').val();
+    var displayLocation = "";
+    var googleLat = 0.0;
+    var googleLng = 0.0;
 
-    console.log(data);
-    $(".city").append("<li>" + city + "</li>")
-    $(".list").append("<li>" + googleLat + "</li>")
-    $(".list").append("<li>" + googleLng + "</li>")
-    $("#submit").on('click', function () {
-    });
-    $("#submit").click(function () {
-
-    });
+     $('#zip').val('');
+  
   });
- });
+
+
+
+
